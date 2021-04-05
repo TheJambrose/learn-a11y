060e7184fa5d5f65c49b73634297405c612acfa2
Jambrose <ambrose_john@yahoo.com>
2021-04-05 18:02:56 -0500

    Add tab trapping and Esc key usage to modal interactions

--- js/modal.js
+++ js/modal.js
@@ -10,13 +10,47 @@ cancelButton.addEventListener('click', close);
 // https://github.com/jkup/focusable
 
 function open() {
-  // Show the modal and overlay
-  modal.style.display = 'block';
-  modalOverlay.style.display = 'block';
-}
 
-function close() {
+	// Show the modal and overlay
+	modal.style.display = 'block';
+	modalOverlay.style.display = 'block';
+	modal.addEventListener('keydown', trap);
+
+	var previousFocus = document.activeElement;
+
+	var focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, [tabindex^="0"], [tabindex^="1"], [tabindex^="2"], [tabindex^="3"], [tabindex^="4"], [tabindex^="5"], [tabindex^="6"], [tabindex^="7"], [tabindex^="8"], [tabindex^="9"]');
+	focusableElements = Array.prototype.slice.call(focusableElements);
+
+	var firstItem = focusableElements[0];
+	var lastItem = focusableElements[focusableElements.length - 1];
+
+	function trap(event) {
+		if (event.keyCode === 9) {
+			//if shift is held
+			if (event.shiftKey) {
+				//tabbing backwards
+				if (document.activeElement === firstItem) {
+					event.preventDefault();
+					lastItem.focus();
+				}
+			} else {
+				// tabbing forward (if Shift is not held)
+				if (document.activeElement === lastItem) {
+					event.preventDefault();
+					firstItem.focus();
+				}
+			}
+		} else if (event.keyCode === 27) {
+				//check for Esc Key and close modal
+				close();
+			}
+		}
+
+	}
+
+	function close() {
   // Hide the modal and overlay
   modal.style.display = 'none';
   modalOverlay.style.display = 'none';
+
 }
